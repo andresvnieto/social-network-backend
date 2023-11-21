e4cb3240ad32812b93dbe23b85aae4b5895a1e2a
andresvnieto <andreswd001@gmail.com>
2023-11-20 21:40:24 -0500

    swagger Docs  añadido

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { router } from "./components/user/network.js";
 import config from "../config.js";
+import swaggerUI from "swagger-ui-express";
+import swaggerDocument from "./swagger.json" assert { type: "json" };
 
 const app = express();
 
@@ -8,6 +10,7 @@ app.use(express.json());
 
 // ROUTER
 app.use("/api/user", router);
+app.use("/api/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.listen(config.api.port, () => {
   console.log("Api escuchando en el puerto: " + config.api.port);
